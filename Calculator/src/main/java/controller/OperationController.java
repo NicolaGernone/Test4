55eab0a0d2dcab5f08c69787aa65202b7857a19f
NicolaGernone <nicola.gernone02@gmail.com>
2019-12-19 12:28:14 +0100

    ajustes codigo y agregacion html para template calculadora

--- Calculator/src/main/java/controller/OperationController.java
+++ Calculator/src/main/java/controller/OperationController.java
@@ -3,10 +3,12 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import io.corp.calculator.TracerImpl;
 import model.OperationModel;
+import services.OperationServices;
 
 @Controller
 public class OperationController extends TracerImpl{
@@ -15,7 +17,7 @@ public class OperationController extends TracerImpl{
 	OperationModel modelBean = new OperationModel();
 	
 	@Autowired
-	private OperationController operationController;
+	private OperationServices operationServices;
 	
 	/**
 	 * map del xml para la interface de la calculadora
@@ -24,8 +26,34 @@ public class OperationController extends TracerImpl{
 	 */
 	@RequestMapping("/")
     private String getCalculatorPage(Model model){
-        model.addAttribute("operationBean",modelBean);
+        model.addAttribute("operationModel",modelBean);
+        trace(model);
         return "calculator";
 	}
+	
+	/**
+	 * servicio para suma
+	 * @param form
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/", params="add", method = RequestMethod.POST)
+    private String add(@ModelAttribute("operationModel")  OperationModel modelBean, Model model ){
+        model.addAttribute("result", operationServices.add(modelBean));
+        return "calculator";
+    }
+	
+	/**
+	 * servicio para resta
+	 * @param form
+	 * @param model
+	 * @return
+	 */
+    @RequestMapping(value="/", params="subtract", method = RequestMethod.POST)
+    private String subtract(@ModelAttribute("operationBean")  OperationModel modelBean, Model model ){
+        model.addAttribute("result", operationServices.subtr(modelBean));
+        return "calculadora";
+    }
+	
 
 }
